ccbbceba2c7b7683a7d3a1eb98624c4f7bfb1e35
csharp <shipci@outlook.com>
2014-11-17 14:49:39 +0530

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,7 +3,7 @@ require_once('Example.php');
 class ExampleTest extends PHPUnit_Framework_TestCase {
   public function testExample() {
   	$exmp = new Example();
-    $this->assertEquals(strpos($exmp->test(), 'h') == 2, true);
+    $this->assertEquals(strpos($exmp->test(), 'h') == 1, true);
   }
 }
 ?>
